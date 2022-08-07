43ef8f5a65dac3a65c37fee85a21cba33d83f9a1
Nick <Npgb18@gmail.com>
2022-08-07 19:13:02 +0100

    Game play set up

--- Character.js
+++ Character.js
@@ -1,27 +1,44 @@
-import {getDiceRollArray} from '/utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml, getPercentage} from './utils.js'
 
 function Character(data) {
     Object.assign(this, data)
     
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
-        }).join('')
-    }
+    this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+    this.maxHealth = this.health
 
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
-        let diceHtml = this.getDiceHtml(diceCount);
-        
+    this.setDiceHtml = () => {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map((num) => `<div class="dice">${num}</div>`).join('')
+    } 
+    this.takeDamage = (attackScoreArray) => {
+        this.health -= attackScoreArray.reduce((total, next) => total + next)
+        if (this.health < 0) {
+            this.dead = true
+            this.health = 0
+        }
+    }
+    this.getHealthBarHtml = () => {
+        const percent = getPercentage(this.maxHealth, this.health)
+        const lowHealth = percent < 26 ? 'danger' : ''
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${lowHealth} " 
+                        style="width: ${percent}%;">
+                    </div>
+                </div>`
+    }
+    this.getCharacterHtml = () => {
+        const { elementId, name, avatar, health, diceCount, diceHtml } = this
+        const healthBar = this.getHealthBarHtml()          
            return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${diceHtml}
                 </div>
-            </div>`;
+            </div>`
     }  
 }
 
--- utils.js
+++ utils.js
@@ -1,7 +1,14 @@
 function getDiceRollArray(diceCount) {  
-    return new Array(diceCount).fill(0).map(function(){
-    return Math.floor(Math.random() * 6) + 1
-    });   
+    return new Array(diceCount).fill(0).map(() => Math.floor(Math.random() * 6) + 1)  
 }
 
-export {getDiceRollArray}
+function getDicePlaceholderHtml(diceCount) {
+    return new Array(diceCount).fill(0).map(() => `<div class="placeholder-dice"></div>`).join('')
+}
+
+function getPercentage(maxHealth, remainHealth) {
+    return Math.floor((remainHealth / maxHealth) * 100)
+}
+
+
+export {getDiceRollArray, getDicePlaceholderHtml, getPercentage}
