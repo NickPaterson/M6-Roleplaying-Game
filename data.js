0a8802ff94271cfa0a2561b7b5c3eb507c7d4769
Nick <Npgb18@gmail.com>
2022-08-07 09:00:15 +0100

    DOM IDs hardcoded

--- data.js
+++ data.js
@@ -1,13 +1,11 @@
 const characterData = {
     hero: {
-        elementId: "hero",
         name: "Wizard",
         avatar: "images/wizard.png",
         health: 60,
         diceRoll: 6
      },
      monster: {
-        elementId: "monster",
         name: "Orc",
         avatar: "images/orc.png",
         health: 10,
